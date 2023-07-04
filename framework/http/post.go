4a73c4a13a5f10060510615e9c14344f18d05b5f
huangdeyong <huangdeyong@qq.com>
2023-07-04 17:26:34 +0800

    post支持body为json格式

--- framework/http/post.go
+++ framework/http/post.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -24,6 +25,33 @@ func Post(url string, data interface{}, result interface{}, header map[string]st
 		for k, v := range header {
 			request.Header.Add(k, v)
 		}
+	}
+	var client = http.Client{Timeout: 5 * time.Second}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	errs := json.Unmarshal(body, &result)
+	if err != nil {
+		return errs
+	}
+	fmt.Println(result)
+	return nil
+}
+
+func Post2(url string, data string, result interface{}, header map[string]string) error {
+	request, err := http.NewRequest(http.MethodPost, url, strings.NewReader(data))
+	if err != nil {
+		return err
+	}
+	//request.Header.Add("Content-Type", "application/json")
+	for k, v := range header {
+		request.Header.Add(k, v)
 	}
 	var client = http.Client{Timeout: 5 * time.Second}
 	response, err := client.Do(request)
