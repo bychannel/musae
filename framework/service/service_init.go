d7b0d43ab4311b7e52d66f5a9d7492654635c9a1
bychannel <1132431868@qq.com>
2023-09-12 19:48:06 +0800

    修正日志文件名格式
    (cherry picked from commit 2c62feb1c4e9fb200c109679bf4d8709c5d75bd3)

--- framework/service/service_init.go
+++ framework/service/service_init.go
@@ -87,9 +87,9 @@ func (s *Service) InitLog() error {
 	var fileName string
 	if global.Env != global.ENV_PC {
 		//fileName = fmt.Sprintf("%s-%v-%v", s.AppId, global.SID, time.Now().Format("2006-01-02-15-04-05"))
-		fileName = fmt.Sprintf("%s-%v", global.HostName, time.Now().Format("2006-01-02"))
+		fileName = fmt.Sprintf("%s", global.HostName /*, time.Now().Format("2006-01-02")*/)
 	} else {
-		fileName = fmt.Sprintf("%s-%s", s.AppId, time.Now().Format("2006-01-02"))
+		fileName = fmt.Sprintf("%s", s.AppId /*, time.Now().Format("2006-01-02")*/)
 	}
 	if err := logger.Init(s.LogDir+"/plog", fileName); err != nil {
 		return err
